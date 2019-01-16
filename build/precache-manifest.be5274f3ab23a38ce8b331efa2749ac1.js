5aec294ddccf4b218159e87ec54fdf155ed4c17e
PANDAmonium <jazuma86@yahoo.com>
2019-01-15 18:34:23 -0800

    added basic folder and file structure

---
+++ build/precache-manifest.be5274f3ab23a38ce8b331efa2749ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83d0c358eb7bc9b853ad",
+    "url": "/static/js/main.83d0c358.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "83d0c358eb7bc9b853ad",
+    "url": "/static/css/main.1a46ba48.chunk.css"
+  },
+  {
+    "revision": "90dc74eb277fb7b5038a5d17aebc6554",
+    "url": "/index.html"
+  }
+];
